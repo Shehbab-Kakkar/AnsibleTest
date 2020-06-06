267b7f3a80a1b7b52b19f53e89a20e8858eb5e50
Shehbab-Kakkar <shehbab1@gmail.com>
2020-06-06 19:24:16 +0530

    Create puppet-site.pp

---
+++ Puppet/puppet-site.pp
@@ -0,0 +1,23 @@
+Using site.pp
+server# cd /etc/puppet/environments/production/
+production# vim site.pp
+node default {
+  notify {"This is the default node":}
+}
+
+production# ls
+environment.conf  manifests modules site.pp
+
+# mv site.pp manifests/
+
+node2 # puppet agent -t
+
+-------
+# site.pp
+node default {
+  notify {"This is the default node":}
+}
+node 'node1.example.com', 'node2.example.com' {
+  notify {"This is the multi node defination":}
+}
+--------
